d684d19bc69f10b565d3d15be91407a7b689ef45
Christoph Jansen <Christoph.Jansen@htw-berlin.de>
2019-06-28 19:57:13 +0200

    calculate_number_of_signals

--- edfrd/__init__.py
+++ edfrd/__init__.py
@@ -1,9 +1,10 @@
+import os
 from collections import namedtuple
 import warnings
 import numpy as np
 
 
-__version__ = '0.5'
+__version__ = '0.6'
 
 
 def _str(f, size, _):
@@ -33,7 +34,7 @@ def _discard(f, size, _):
     f.read(size)
 
 
-_HEADER = [
+HEADER = (
     ('version', 8, _str),
     ('local_patient_identification', 80, _str),
     ('local_recording_identification', 80, _str),
@@ -44,10 +45,10 @@ _HEADER = [
     ('number_of_data_records', 8, _int),
     ('duration_of_a_data_record', 8, _int),
     ('number_of_signals', 4, _int),
-]
+)
 
 
-_SIGNAL_HEADER = [
+SIGNAL_HEADER = (
     ('label', 16, _str),
     ('transducer_type', 80, _str),
     ('physical_dimension', 8, _str),
@@ -58,24 +59,53 @@ _SIGNAL_HEADER = [
     ('prefiltering', 80, _str),
     ('nr_of_samples_in_each_data_record', 8, _int),
     ('reserved', 32, _discard)
-]
+)
 
 
-Header = namedtuple('Header', [name for name, _, _ in _HEADER] + ['signals'])
-SignalHeader = namedtuple('SignalHeader', [name for name, _, _ in _SIGNAL_HEADER])
+INT_SIZE = 2
+HEADER_SIZE = sum([size for _, size, _ in HEADER])
+SIGNAL_HEADER_SIZE = sum([size for _, size, _ in SIGNAL_HEADER])
 
 
-def read_header(file_path):
+Header = namedtuple('Header', [name for name, _, _ in HEADER] + ['signals'])
+SignalHeader = namedtuple('SignalHeader', [name for name, _, _ in SIGNAL_HEADER])
+
+
+def read_header(file_path, calculate_number_of_data_records=None):
     with open(file_path, 'rb') as f:
-        header = [func(f, size, name) for name, size, func in _HEADER]
+        header = [func(f, size, name) for name, size, func in HEADER]
         number_of_signals = header[-1]
         signal_headers = [[] for _ in range(number_of_signals)]
 
-        for name, size, func in _SIGNAL_HEADER:
+        for name, size, func in SIGNAL_HEADER:
             for signal_header in signal_headers:
                 signal_header.append(func(f, size, name))
 
     header.append(tuple((SignalHeader(*signal_header) for signal_header in signal_headers)))
+
+    if calculate_number_of_data_records:
+        data_record_size = sum([signal.nr_of_samples_in_each_data_record for signal in header[-1]]) * INT_SIZE
+
+        file_size_without_headers = os.path.getsize(file_path) - (HEADER_SIZE + SIGNAL_HEADER_SIZE * number_of_signals)
+
+        if file_size_without_headers % data_record_size != 0:
+            warnings.warn('file_size_without_headers {fswh} is not a multiple of data_record_size {drs}'.format(
+                fswh=file_size_without_headers,
+                drs=data_record_size
+            ))
+
+        calculated_number_of_data_records = file_size_without_headers // data_record_size
+        number_of_data_records = header[7]
+
+        if calculated_number_of_data_records != number_of_data_records:
+            warnings.warn(
+                'number_of_data_records {n} in header does not match calculated number_of_data_records {cn}'.format(
+                    n=number_of_data_records,
+                    cn=calculated_number_of_data_records
+                )
+            )
+
+        header[7] = calculated_number_of_data_records
 
     return Header(*header)
 
@@ -83,13 +113,10 @@ def read_header(file_path):
 def read_data_records(file_path, header, start=None, end=None):
     start = start if start is not None else 0
     end = end if end is not None else header.number_of_data_records
-    int_size = 2
-    header_size = sum([size for _, size, _ in _HEADER])
-    signal_header_size = sum([size for _, size, _ in _SIGNAL_HEADER])
     data_record_length = sum([signal.nr_of_samples_in_each_data_record for signal in header.signals])
 
     with open(file_path, 'rb') as f:
-        f.seek(header_size + header.number_of_signals * signal_header_size + start * data_record_length * int_size)
+        f.seek(HEADER_SIZE + header.number_of_signals * SIGNAL_HEADER_SIZE + start * data_record_length * INT_SIZE)
 
         for _ in range(start, end):
             a = np.fromfile(f, count=data_record_length, dtype=np.int16)
